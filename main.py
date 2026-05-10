e787a2945deeee9d81965b6ba0a98071e26b9a75
-𝙔𝙐𝙑𝙄  🩷 <yuvixhenrydon@gmail.com>
2026-05-10 11:39:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ body {
 /* Satik Bio Panel */
 .info-panel {
   width: 100%;
-  background: rgba(255, 255, 255, 0.05);
+  background: gray;
   backdrop-filter: blur(20px);
   -webkit-backdrop-filter: blur(20px);
   border: 1px solid rgba(255, 255, 255, 0.1);
